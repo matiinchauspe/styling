f75db6eec0c4041ab156a2d815ccc4b3029e5e0c
Matias Inchauspe <mati.inchauspe@gmail.com>
2024-04-26 10:29:25 -0300

    style: add tweaks after review

--- src/components/firm-facts/firm-facts.tsx
+++ src/components/firm-facts/firm-facts.tsx
@@ -1,13 +1,19 @@
+import { FC } from "react";
+
 import { Icons } from "@/components/icons";
 import classes from "./firm-facts.module.scss";
 import { GridLayout } from "./grid-layout";
 
-const FirmFacts = () => (
+interface FirmFactsProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const FirmFacts: FC<FirmFactsProps> = () => (
   <div className={classes.container}>
     <Icons.Close className={classes.closeIcon} />
-    {/* title */}
-    <h2 className={classes.title}>Firm Facts</h2>
-    <div className={classes.br} />
+    {/* header */}
+    <div className={classes.header}>
+      <h2 className={classes.title}>Firm Facts</h2>
+      <div className={classes.br} />
+    </div>
     {/* layout */}
     <GridLayout />
   </div>
